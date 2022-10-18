50a7c6a82b95a6e8164e4eaf0b9261d483e73b7f
elhachimi.ch <elhachimi.ch@gmail.com>
2022-10-18 15:56:55 +0100

    Climate class

---
+++ climatefiller.py
@@ -0,0 +1,17 @@
+
+class ClimateFiller():
+    
+    def __init__(self):
+        pass
+    
+    def download_era5(self):
+        pass
+    
+    def missing_data_checking(self):
+        pass
+    
+    def fill(self, climate_varibale='Air temperature'):
+        pass
+    
+    def evaluate_products(self):
+        pass
