cabd0f4d2b37abd671bc05f3de530ca110bd01f3
FIRMA-PLATFORM <firma.platform.project@gmail.com>
2023-03-22 01:36:29 +0000

    temp files

--- temp_test.py
+++ temp_test.py
@@ -6,12 +6,18 @@ from climatefiller import ClimateFiller
 def main():
     ti = time.time()
     
+    # Read the time series 
+    data = DataFrame(r"E:\projects\data\r3_weather_data\R3_meteo_N0_Unification.xlsx", data_type='xls', sheet_name='R3_2013-2020')
+    
+    data.keep_columns(['R3_Tair', 'DateBis'])
+
+    # Rename target colmn 
+    data.rename_columns({'R3_Tair':'ta'})
 
     # Initilize the ClimateFiller object
-    climate_filler = ClimateFiller()
+    climate_filler = ClimateFiller(data.get_dataframe(), data_type='df', datetime_column_name='DateBis')
 
-    climate_filler.download('ws', '2020-01-01', '2020-02-01')
-    climate_filler.plot_column('ws')
+    climate_filler.plot_column('ta')
 
    
     print(time.time() - ti)
