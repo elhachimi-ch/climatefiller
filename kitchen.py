66d0775ad01172812c375de95d94f20b1c9a1307
elhachimi-ch <elhachimi.ch@gmail.com>
2023-06-14 16:20:55 +0100

    test gee

--- kitchen.py
+++ kitchen.py
@@ -8,6 +8,13 @@ def main():
     
     # Read the time series 
     data = DataFrame("data/los_angele_sair_temperature.csv")
+    
+    climate_filler = ClimateFiller()
+    
+    #climate_filler.download('ta', backend='gee', start_date='2000-05-01', end_date='2002-05-01') 
+    #climate_filler.download('rs', backend='gee', start_date='2001-01-01', end_date='2001-01-05') 
+    climate_filler.download('ws', backend='gee', start_date='2000-05-01', end_date='2001-07-01') 
+    #climate_filler.download('rh', backend='gee', start_date='2000-05-01', end_date='2001-07-01') 
 
     # Rename target colmn 
     data.rename_columns({'air_temperature':'Ta'})
