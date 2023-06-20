eef59903f90feb1226ed035987d829c7eb8dd29c
elhachimi-ch <elhachimi.ch@gmail.com>
2023-06-20 17:22:44 +0100

    make root_path dynamic

--- source/conf.py
+++ source/conf.py
@@ -10,14 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("D:/projects/climatefiller/"))
+root_path = os.path.dirname(os.path.abspath('.'))
+sys.path.insert(0, root_path)
+
+print(root_path)
 
 extensions = [
     'sphinx.ext.autodoc',
