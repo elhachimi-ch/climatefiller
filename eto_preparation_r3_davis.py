d3725c0e780bd5d5fd13716cc2b083eaee118389
elhachimi-ch <elhachimi.ch@gmail.com>
2024-10-29 19:55:11 +0100

    Refactor code to update latitude and longitude in eto_preparation_r3_davis.py

--- eto_preparation_r3_davis.py
+++ eto_preparation_r3_davis.py
@@ -8,13 +8,27 @@ def main():
     
     # Initilize the ClimateFiller object
     climate_filler = ClimateFiller(
-        r"C:\Users\elhac\OneDrive - Université Mohammed VI Polytechnique\crsa\data\aws_al_haouz\r3_full.csv",
-        lon=-7.60655,
-        lat=31.65936,
+        r"C:\Users\elhac\OneDrive\Desktop\kitchen\projects\pythonsnippets\data\california\cimis_data_hourly.csv",
+        lon=-121.776360,
+        lat=38.535694,
         )
-    
+    # for Davis
+    climate_filler.data.drop_column('et0_pm')
     climate_filler.et0_estimation()
-    climate_filler.et0_output_data.show()
+    data_eto_pm = DataFrame(climate_filler.et0_output_data.dataframe, 'df')
+    climate_filler.et0_estimation(method='hs',)
+    eto_hs = climate_filler.et0_output_data.dataframe['et0_hs']
+    
+    climate_filler.extraterrestrial_radiation_daily()
+    ra = climate_filler.data.resample_timeseries()['ra']
+    
+    data_eto_pm.add_column('eto_hs', eto_hs)
+    print(ra)
+    data_eto_pm.show()
+    data_eto_pm.add_column('ra', ra)
+    data_eto_pm.export('data/medgu/davis_eto_pm_hs_ra.csv')
+    
+    
     
     # HS calibration for R3: a=11.3, b=0.39, C=0.0035
     # R3 "C:\Users\elhac\OneDrive - Université Mohammed VI Polytechnique\crsa\data\aws_al_haouz\r3_full.csv"
